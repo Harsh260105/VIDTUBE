111fb08e52116edeb508f4f3e8fc0715e5d8565c
Harsh260105 <harshpatel17079@gmail.com>
2025-02-11 15:04:02 +0530

    add video route and implement video fetching with filtering and pagination

--- src/app.js
+++ src/app.js
@@ -18,13 +18,15 @@ app.use(express.static('public'));
 app.use(cookieParser());
 
 //imports routes
+import { errorHandler } from './middlewares/error.middlewares.js';
 import healthcheckRouter from './routes/healthcheck.routes.js';
 import userRouter from './routes/user.routes.js'
-import { errorHandler } from './middlewares/error.middlewares.js';
+import videoRouter from './routes/video.routes.js'
 
 //routes
 app.use('/api/v1/healthcheck', healthcheckRouter);
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/video', videoRouter)
 
 app.use(errorHandler)
 
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,6 +1,6 @@
 import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
+import {Video} from "../models/video.models.js"
+import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -10,6 +10,49 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO : get all videos based on query, sort, pagination
+    
+    const filter = {}
+
+    if(query) {
+        filter.$or = [
+            { title: { $regex: query, $options: "i"} },
+            { description: { $regex: query, $options: "i"} },
+        ]
+    }
+
+    if(userId) {
+        filter.owner = userId;
+    }
+
+    const sortOptions = {};
+    sortOptions[sortBy] = sortType === 'asc' ? 1 : -1;
+
+    try {
+        const aggregationPipeline = Video.aggregate([
+            { $match: filter },
+            { $sort: sortOptions }
+        ])
+        
+        const options = {
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10)
+        }
+
+        const videos = await Video.aggregatePaginate(aggregationPipeline, options);
+
+        if(!videos || videos.docs.length === 0) {
+            throw new ApiError(404, "No videos found")
+        }
+
+        return res
+            .status(200)
+            .json( 
+                new ApiResponse(200, videos[0], "Videos fetched successfully")
+            )
+
+    } catch (error) {
+        throw new ApiError(error.statusCode || 500 , error.message || "Error fetching videos")
+    }
 
 })
 
