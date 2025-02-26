0673b7fb4da49dc343e6be2e9911dfa23d50b533
Harsh260105 <harshpatel17079@gmail.com>
2025-02-27 00:26:19 +0530

    feat: add playlist management functionality with routes and controller methods

--- src/app.js
+++ src/app.js
@@ -22,11 +22,13 @@ import { errorHandler } from './middlewares/error.middlewares.js';
 import healthcheckRouter from './routes/healthcheck.routes.js';
 import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
 
 //routes
 app.use('/api/v1/healthcheck', healthcheckRouter);
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/video', videoRouter)
+app.use('/api/v1/playlist', playlistRouter)
 
 app.use(errorHandler)
 
