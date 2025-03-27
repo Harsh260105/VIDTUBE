43550fb80c35292ba0344bcd32143b8da72462a8
Harsh260105 <harshpatel17079@gmail.com>
2025-03-27 21:32:28 +0530

    docs: add detailed comments to JWT verification middleware for clarity

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -3,6 +3,16 @@ import { User } from "../models/user.models.js"
 import { ApiError } from '../utils/ApiError.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
 
+// Middleware to verify JWT token 
+// and attach user to request object
+// It checks for the token in the request cookies or the Authorization header
+// If the token is not present, it throws an Unauthorized error
+// If the token is present, it verifies the token using the secret key
+// and retrieves the user from the database
+// If the user is not found, it throws an Unauthorized error
+// If the user is found, it attaches the user to the request object
+// and calls the next middleware in the stack
+
 export const verifyJWT = asyncHandler( async (req, _, next) => {
     
     const token = req.cookies.accessToken || req.header('Authorization')?.replace("Bearer ", "")
